d32d7cd39afbdffd2f438eed45c5894ff494bb38
Michael Wycombe <michael.wycombe@gmail.com>
2024-11-16 18:55:07 -0800

    Finally got it working on the mac

--- PyQtTraining/twowindows.py
+++ PyQtTraining/twowindows.py
@@ -12,7 +12,6 @@ class form1(qtw.QWidget, Ui_SS_form_1):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-
         self.pb_changeText.clicked.connect(self.button_change_text)
 
     @qtc.Slot()
@@ -23,21 +22,22 @@ class form1(qtw.QWidget, Ui_SS_form_1):
     @qtc.Slot()
     def button_change_text(self):
         self.form2 = form2()
-        self.form2.setupUi(self.form2)
-        # print ('instantiate form2')
+
+        print ('instantiate form2')
         self.form2.submitted.connect(self.lb_changedText.setText)
         self.form2.show()
-        # print ('button-changed-text')
+        print ('button-changed-text')
 
 
 class form2(qtw.QWidget, Ui_SS_form_2):
 
     submitted = qtc.Signal(str)
 
-    def __init_(self):
+    def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.pb_submitText.clicked.connect(self.button_submit)
+
 
     def button_submit(self):
         print ('submit text pressed')
