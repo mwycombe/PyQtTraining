c707628cc52a9180b9d6b1ab51105b9946b9685b
Michael Wycombe <michael.wycombe@gmail.com>
2025-01-19 17:41:33 -0800

    Add crtlVariables

--- PyQtTraining/ValidationTest.py
+++ PyQtTraining/ValidationTest.py
@@ -5,13 +5,42 @@ from PySide6 import QtWidgets as qtw
 from PySide6 import QtGui as qtg
 from PySide6.QtGui import QIntValidator
 from ui_validationwindow import Ui_ValidationWindow
+from ctrlVariables import StringVar, IntVar, DoubleVar
 
 class MainWindow(qtw.QMainWindow, Ui_ValidationWindow):
     def __init__(self):
         super().__init__()
+        self.setupUi(self)
         self.show()
 
+        self.int1 = IntVar()
+        self.int2 = IntVar()
+        self.int3 = IntVar()
 
+
+        self.pb_1.clicked.connect(self.vb_1)
+        self.pb_2.clicked.connect(self.vb_2)
+        self.pb_3.clicked.connect(self.vb_3)
+
+        self.v1 = QIntValidator(1,15,self)
+        self.le_1.setValidator(self.v1)
+
+        self.v2 = QIntValidator(0,9,self)
+        self.le_2.setValidator(self.v2)
+
+        self.le_1.returnPressed.connect.
+
+    def vb_1(self):
+        print('vb_1')
+        # vb_1 action
+
+    def vb_2(self):
+        print('vb_2')
+        # vb_2 action
+
+    def vb_3(self):
+        print('vb_3')
+        # vb_3 action
 
 if __name__ == "__main__":
     app = qtw.QApplication(sys.argv)
